452f165d74190cd25ca2a3cf789459b84047aa89
KshithijSinghania <kshithijsinghania@gmail.com>
2025-12-11 21:16:35 +0530

    Add implementations for A. Operations with Inversions and B. Optimal Shifts problems from Codeforces Round 1070

---
+++ A_Operations_with_Inversions.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long int
+#define mod 1000000007
+#define reverse(v) reverse(v.begin(), v.end())
+#define sort1(v) sort(v.begin(), v.end())
+#define ld long double
+#define ull unsigned long long int
+#define loop(i, n) for (ll i = 0; i < n; i++)
+#define loop1(i, n) for (ll i = 1; i <= n; i++)
+#define loopr(i, n) for (ll i = n - 1; i >= 0; i--)
+#define loopr1(i, n) for (ll i = n; i > 0; i--)
+using namespace std;
+
+bool comp(pair<ll, ll> p1, pair<ll, ll> p2)
+{
+    return p1.first > p2.first;
+}
+
+void inp(vector<ll> &a, ll n)
+{
+    loop(i, n)
+    {
+        ll d;
+        cin >> d;
+        a.push_back(d);
+    }
+}
+
+//-------------------------------------
+
+void solve(vector<ll> a, ll n)
+{
+    ll ans = 0;
+    ll maxi = 0;
+    loop(i, n)
+    {
+        if (a[i] < maxi)
+        {
+            ans++;
+        }
+        maxi = max(maxi, a[i]);
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ull t;
+    cin >> t;
+    while (t > 0)
+    {
+        t--;
+        ll n;
+        cin >> n;
+        vector<ll> a;
+        inp(a, n);
+        solve(a, n);
+    }
+}
