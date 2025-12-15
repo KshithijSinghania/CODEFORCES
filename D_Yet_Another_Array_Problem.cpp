5c5960ef57ab157dbe5d16b3baeb1e5082201e1a
KshithijSinghania <kshithijsinghania@gmail.com>
2025-12-15 22:24:11 +0530

    Add implementations for problems B, C, D from Codeforces Round 1062 (Div. 4)

---
+++ D_Yet_Another_Array_Problem.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long int
+#define mod 1000000007
+#define all(x) (x).begin(), (x).end()
+#define ld long double
+#define ull unsigned long long int
+#define loop(i, n) for (ll i = 0; i < n; i++)
+#define loop1(i, n) for (ll i = 1; i <= n; i++)
+#define loopr(i, n) for (ll i = n - 1; i >= 0; i--)
+#define loopr1(i, n) for (ll i = n; i > 0; i--)
+using namespace std;
+
+bool comp(pair<ll, ll> p1, pair<ll, ll> p2)
+{
+    return p1.first > p2.first;
+}
+
+void inp(vector<ll> &a, ll n)
+{
+    loop(i, n)
+    {
+        ll d;
+        cin >> d;
+        a.push_back(d);
+    }
+}
+
+//-------------------------------------
+
+void solve(vector<ll> a, ll n)
+{
+    vector<ll> primes = {
+        2, 3, 5, 7, 11, 13, 17, 19,
+        23, 29, 31, 37, 41, 43, 47, 53};
+
+    for (ll x : primes)
+    {
+        bool ok = false;
+        for (ll i : a)
+        {
+            if (i % x != 0)
+            {
+                ok = true;
+                break;
+            }
+        }
+        if (ok)
+        {
+            cout << x << '\n';
+            return;
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ull t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        vector<ll> a;
+        inp(a, n);
+        solve(a, n);
+    }
+    return 0;
+}
